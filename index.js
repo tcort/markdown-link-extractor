685474fb0b41941aaaa340f228d12f91aa8d5cce
Thomas Cort <thomasc@ssicanada.com>
2024-03-10 13:51:37 -0400

    Revert "Reintroduce anchor detection as configurable step"
    This reverts commit 34a545a50b05dcdd76f34693f174f0573ed8c985.

--- index.js
+++ index.js
@@ -3,33 +3,13 @@
 const { marked } = require('marked');
 const htmlLinkExtractor = require('html-link-extractor');
 
-module.exports = function markdownLinkExtractor(markdown, checkAnchors = false) {
-    const anchors = [];
-    if(checkAnchors) {
-        const renderer = {
-            heading(text, level, raw, slugger) {
-                if (this.options.headerIds) {
-                    var id = this.options.headerPrefix + slugger.slug(raw);
-                    
-                        anchors.push(`#${id}`);
-                    
-                    return "<h" + level + " id=\"" + id + "\">" + text + "</h" + level + ">\n";
-                } // ignore IDs
-
-
-                return "<h" + level + ">" + text + "</h" + level + ">\n";
-            }
-        };
-
-        marked.use({ renderer });
-    }
+module.exports = function markdownLinkExtractor(markdown, extended = false) {
 
     marked.setOptions({
         mangle: false, // don't escape autolinked email address with HTML character references.
     });
 
-
     const html = marked(markdown);
     const links = htmlLinkExtractor(html);
-    return { links, anchors };
+    return links;
 };
